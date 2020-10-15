0a706994323d05e5f9f1f7a1ef2fb0991d044327
dangermyong <66460964+dangermyong@users.noreply.github.com>
2020-10-15 17:57:48 +0900

    first blog

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
+import { Context } from '../context/BlogContext'
+
+const IndexScreen = () => {
+  const { state, addBlogPost } = useContext(Context)
+
+  return (
+    <View>
+      <Text>Index Screen</Text>
+      <Button title='Add Post' onPress={addBlogPost} />
+      <FlatList
+        data={state}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>
+        }}
+      />
+    </View>
+  )
+}
+
+export default IndexScreen
+
+const styles = StyleSheet.create({})
